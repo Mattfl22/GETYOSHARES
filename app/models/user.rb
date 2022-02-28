2258f43433d1271ef7cc4a05fd45c55d75215e38
Matthieu Flecher <flecher.matthieu@gmail.com>
2022-02-28 17:05:23 +0100

    fix on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :projects, :transactions
+  has_many :projects
+  has_many :transactions
   has_many :revenues, through: :projects
 end
