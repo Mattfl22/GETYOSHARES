5eb79870e9ad73d67e04d855b6272cc3d96581bd
Matthieu Flecher <flecher.matthieu@gmail.com>
2022-02-28 16:15:37 +0100

    model for project

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_135349) do
+ActiveRecord::Schema.define(version: 2022_02_28_150407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "distributor"
+    t.string "name"
+    t.string "description"
+    t.date "release_date"
+    t.integer "average_distribution_share"
+    t.integer "expected_audio_streams_year"
+    t.integer "expected_video_streams_year"
+    t.integer "number_of_tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +48,5 @@ ActiveRecord::Schema.define(version: 2022_02_28_135349) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
