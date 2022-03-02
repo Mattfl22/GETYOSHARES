774674d2bbfe30ea05f27978348193e237650620
Vincent Orinel <vincent.orinel@gmail.com>
2022-03-02 16:43:57 +0100

    fixing gemfile lock

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,11 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :token
+  after_create :set_token_as_bought
+
+  def set_token_as_bought
+    return if token.bought?
+
+    token.update(bought: true)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_160312) do
+ActiveRecord::Schema.define(version: 2022_03_02_153352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_160312) do
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "bought"
     t.index ["project_id"], name: "index_tokens_on_project_id"
   end
 
