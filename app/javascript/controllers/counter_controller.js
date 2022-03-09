07ac516c4e8a7dd8e627301dc657ed793d79edd6
Matthieu Flecher <flecher.matthieu@gmail.com>
2022-03-09 10:32:13 +0100

    fix for token max and min values  when purchasing a token

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -2,18 +2,26 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "quantity", "plus", "minus" ]
-  
+
   connect() {
     this.quantity = 0
   }
 
   increment() {
-    this.quantity++;
-    this.quantityTarget.value = this.quantity;
+    var max = quantity.max;
+    if (this.quantity++ < max) {
+      this.quantityTarget.value = this.quantity;
+    } else {
+      this.quantity = max;
+    }
   }
 
   decrement () {
-    this.quantity--;
-    this.quantityTarget.value = this.quantity;
+    var min = 0;
+    if (this.quantity-- > 0) {
+      this.quantityTarget.value = this.quantity;
+    } else {
+      this.quantity = min;
+    }
   }
 }
