a864551c4e93b371fb291a45d295917ead57cfa9
Marie Desjonqueres marie.desjonqueres96@gmail.com <marie.desjonqueres96@gmail.com>
2022-03-09 16:26:02 +0100

    confirmation align payment

--- db/schema.rb
+++ db/schema.rb
@@ -95,8 +95,8 @@ ActiveRecord::Schema.define(version: 2022_03_08_144856) do
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "price_cents", default: 0, null: false
     t.boolean "bought", default: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["project_id"], name: "index_tokens_on_project_id"
   end
 
