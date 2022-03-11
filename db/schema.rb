a4f63f8a77ea070cff5206b564a0a563187dd0a6
Vincent Orinel <vincent.orinel@gmail.com>
2022-03-11 11:50:53 +0100

    final seeds

--- db/schema.rb
+++ db/schema.rb
@@ -95,8 +95,8 @@ ActiveRecord::Schema.define(version: 2022_03_10_141611) do
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "bought", default: false
     t.integer "price_cents", default: 0, null: false
+    t.boolean "bought", default: false
     t.index ["project_id"], name: "index_tokens_on_project_id"
   end
 
